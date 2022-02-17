7c7af9ca375dcba231de3fc92c9c980425121e3e
elodin19 <joanoldaniel@gmail.com>
2022-02-17 16:21:37 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿People people = new People("Daniel Joanol", 98312344, "Paseo de los Deportistas", "joanol@daniel.com", true);
+Console.WriteLine(people);
+
+public struct People{
+
+    public People(string name, int phone, string address, string email, bool newClient){
+        Name = name;
+        Phone = phone;
+        Address = address;
+        Email = email;
+        NewClient = newClient;
+    }
+
+    public string Name { get;}
+    public int Phone { get;}
+    public string Address { get;}
+    public string Email { get;}
+    public bool NewClient { get;}
+
+    public override string ToString() => $"Name: {Name} - Phone: {Phone} - Address: {Address} - Email: {Email} - NewClient: {NewClient}";
+}
